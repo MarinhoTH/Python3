551236041b2b7692a0375cfea584e87394f160e9
Thiago Marinho <100244278+MarinhoTH@users.noreply.github.com>
2022-06-19 20:31:03 -0300

    update

---
+++ automatico/main.py
@@ -0,0 +1,27 @@
+import pyautogui
+from time import sleep
+
+
+pyautogui.alert('Calmo, o código vai começar.')
+pyautogui.pause = 2
+
+
+#Fazendo o checkin diario
+pyautogui.press('winleft')
+sleep(0.5)
+pyautogui.write('op.gg', interval=0.1)
+sleep(5)
+pyautogui.press('enter')
+sleep(10)
+pyautogui.write('League of Legends')
+sleep(0.5)
+pyautogui.press('enter')
+sleep(5)
+
+pyautogui.press('winleft')
+sleep(0.5)
+pyautogui.write('discord', interval=0.3)
+sleep(1)
+pyautogui.press('enter')
+sleep(0.5) 
+
